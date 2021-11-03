f344988f666748d775c66e47d011ef2b2a2eec00
浅陌丿 <1520190873@qq.com>
2021-11-03 19:37:14 +0800

    部署

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import SocketIO from "socket.io-client";
 Vue.use(
   new VueSocketIO({
     debug: true,
-    connection: SocketIO("ws://127.0.0.1:4000"),
+    connection: SocketIO(process.env.VUE_APP_WS),
   })
 );
 Vue.config.productionTip = false;
